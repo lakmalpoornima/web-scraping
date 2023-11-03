9425320969bbe8434b5ddf2c176b23dab072042d
1a11a <86703816+1a11a@users.noreply.github.com>
2023-11-03 21:45:18 +0530

    update

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -2,23 +2,41 @@ import React, { useState, useEffect } from 'react';
 
 export default function Table() {
   const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [editItem, setEditItem] = useState(null);
+  const itemsPerPage = 10;
 
   const fetchDataFromBackend = () => {
     fetch('http://localhost:4000/items/items')
       .then((response) => response.json())
       .then((data) => {
         setItems(data);
-        console.log(data)
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-      console.log(items)
   };
 
   useEffect(() => {
     fetchDataFromBackend();
   }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const openEditModal = (item) => {
+    setEditItem(item);
+    // Add code here to display a modal for editing with form fields pre-filled with item data.
+  };
+
+  const deleteItem = (itemId) => {
+    // Add code here to delete the item with the specified itemId.
+  };
 
   return (
     <div className="container text-center mt-5">
@@ -27,7 +45,7 @@ export default function Table() {
         <thead>
           <tr>
             <th>PId</th>
-            <th>categories </th>
+            <th>categories</th>
             <th>name_brand</th>
             <th>name_i</th>
             <th>name_j</th>
@@ -38,10 +56,11 @@ export default function Table() {
             <th>Pinfo</th>
             <th>Blink</th>
             <th>img_links</th>
+            <th>Action</th> {/* Added for Edit and Delete buttons */}
           </tr>
         </thead>
         <tbody>
-          {items.map((item, index) => (
+          {currentItems.map((item, index) => (
             <tr key={item._id}>
               <td>{item.PId}</td>
               <td>{item.categories}</td>
@@ -53,12 +72,38 @@ export default function Table() {
               <td>{item.Wprice}</td>
               <td>{item.Psp}</td>
               <td>{item.Pinfo}</td>
-              <td>{item.Pinfo2}</td>
               <td>{item.Blink}</td>
+              <td>{item.img_links}</td>
+              <td>
+                <button
+                  onClick={() => openEditModal(item)}
+                  className="btn btn-primary"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => deleteItem(item._id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div>
+        <ul className="pagination">
+          {Array.from({ length: Math.ceil(items.length / itemsPerPage) }, (_, i) => (
+            <li key={i} className={`page-item ${i + 1 === currentPage ? 'active' : ''}`}>
+              <button onClick={() => paginate(i + 1)} className="page-link">
+                {i + 1}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
